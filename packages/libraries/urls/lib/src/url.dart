d3cfced9c0beeb4ae1462d0b7428be8e43e2dbad
Usman Khan <usman.khan@evantagesoft.com>
2024-01-27 14:35:24 +0500

    add header and token variables to Urls module.

--- packages/libraries/urls/lib/src/url.dart
+++ packages/libraries/urls/lib/src/url.dart
@@ -1,3 +1,8 @@
 class Urls {
   static const String baseUrl = 'https://donation4all.com/api';
+  static String token = '';
+  static var headers = <String, String>{
+    "Content-Type": "application/json",
+    // 'auth': token,
+  };
 }
